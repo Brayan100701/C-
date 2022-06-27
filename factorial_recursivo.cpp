fb6d0031f3b74ec812acd196e252dd0d8bb23e99
Brayan Benjamín <102554530+Brayan100701@users.noreply.github.com>
2022-06-27 12:54:26 -0500

    Add files via upload

---
+++ factorial_recursivo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <limits>
+#include <conio.h>
+
+int val_num(int);
+int factorial(int);
+
+int main(){
+	int num,fact=1;
+	std::cout<<"Ingresa un numero: ";
+	num = val_num(num);
+	
+	fact = factorial(num);
+		
+	std::cout<<"El factorial de "<<num<<" es: "<<fact<<std::endl;
+	
+	return 0;
+}
+
+int val_num(int num){
+	while(!(std::cin>>num) || num<1){
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+		std::cout<<"Cantidad no valida, intentelo de nuevo: ";
+	}
+	return num;
+}
+
+int factorial(int num){
+	if(num>1)
+		num = num*factorial(num-1);
+	return num;
+}
